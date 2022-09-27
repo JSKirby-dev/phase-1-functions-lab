28c0479929fdeb2b33fd78aae4e5b63821c08036
soloman <solomankirby@yahoo.com>
2022-09-27 17:19:49 +1000

    commit tree ffdc599e11e20438335df6156fc9745d89850083

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location){
+  const hq = 42;
+
+  if (location > hq){
+    return location - hq;
+  }else {
+    return hq - location;
+  }
+}
+function distanceFromHqInFeet(location){
+    return distanceFromHqInBlocks(location) * 264;
+}
+function distanceTravelledInFeet(start, dest){
+ if (start > dest){
+    return (start - dest) * 264;
+
+ }else{
+    return (dest - start) * 264;
+ }
+
+}
